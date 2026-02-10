985d3367a67e52abeb792f7dba1dda5bb5187531
Brrn91 <lucasbruno81@gmail.com>
2026-02-10 20:26:29 -0300

    desafio finalizado

--- curso-em-video/desafios/d001/main.py
+++ curso-em-video/desafios/d001/main.py
@@ -1,2 +1,13 @@
 nome = input('Qual é o seu nome?')
 print('Olá', nome , 'seja bem vindo!')
+
+dia = input('Que dia você nasceu?')
+mes = input('De que mês?')
+ano = input('Qual o ano?')
+
+print(nome ,'você nasceu no dia', dia, 'de', mes, 'de', ano)
+
+num1 = int(input('Digine o primeiro número:'))
+num2 = int(input('Digite o segundo número:'))
+soma = num1 + num2
+print(num1 + num2)
