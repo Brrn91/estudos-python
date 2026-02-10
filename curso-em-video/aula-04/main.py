04594a9cdcd1cd8c7df0fb4b9174099ddb6eede9
Brrn91 <lucasbruno81@gmail.com>
2026-02-10 20:10:05 -0300

    Create main.py

---
+++ curso-em-video/aula-04/main.py
@@ -0,0 +1,14 @@
+# print('Olá, Mundo!')
+# print(7+4)
+# print('7' + '4')
+# print('Olá' , 5)
+
+# nome = 'Lucas'
+# idade = 28
+# peso = 70.5
+
+# print(nome, idade, peso)
+
+nome = input('Qual é o seu nome?')
+idade = input('Quantos anos você tem?')
+peso = input('Qual é o seu peso?')
